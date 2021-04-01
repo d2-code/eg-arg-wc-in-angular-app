e2cc92cdc5d223d14e2e5874e50d0aaf0e9dc6a7
thomaswford <tford@nflc.umd.edu>
2021-04-01 09:15:15 -0400

    wip

--- src/directives/proxies.ts
+++ src/directives/proxies.ts
@@ -6,15 +6,34 @@ import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 import { Components } from '@d2-code/wc-lib';
 
 
+export declare interface D2ArgEditor extends Components.D2ArgEditor {}
+@ProxyCmp({
+  inputs: ['data', 'edit']
+})
+@Component({
+  selector: 'd2-arg-editor',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  inputs: ['data', 'edit']
+})
+export class D2ArgEditor {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
+}
+
+
 export declare interface D2Argument extends Components.D2Argument {}
 @ProxyCmp({
-  inputs: ['data']
+  inputs: ['data', 'edit']
 })
 @Component({
   selector: 'd2-argument',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
-  inputs: ['data']
+  inputs: ['data', 'edit']
 })
 export class D2Argument {
   protected el: HTMLElement;
